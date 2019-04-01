288ac3858987d3f8a57d44b31b55254f6c836d08
Ben R F <benramsayfoster@gmail.com>
2019-04-01 11:37:47 +0100

    Fixed issue with checking if a certain creep should be built

--- build.unit.check.js
+++ build.unit.check.js
@@ -32,15 +32,15 @@ module.exports = {
             buildUnit.run(['harvester',1,2,2,0,0,0,0,0],spawner);
         }
         for (var i=0; i < num; i++) {
-            if (harvesters.length - i == 0) {
+            if (harvesters.length - i <= 0) {
                 suggested = harvester;
-            } else if(builders.length - i == 0) {
+            } else if(builders.length - i <= 0) {
                 suggested = builder;
-            } else if(upgraders.length - i == 0) {
+            } else if(upgraders.length - i <= 0) {
                 suggested = upgrader;
             } else if(transporters.length < 1 && containers.length >= 1 && extensions.length >= 1) {
                 suggested = transporter;
-            } else if(fixers.length - i == -2) {
+            } else if(fixers.length - i <= -2) {
                 suggested = fixer;
             }
         }
